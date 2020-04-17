c5919e52f26b2a472b6d4ba52428e53d4580c3c4
Santiago Hermo <santiagohermo@users.noreply.github.com>
2020-04-17 08:34:47 -0700

    # base code kickoff

--- base/code/wages_employment.py
+++ base/code/wages_employment.py
@@ -2,6 +2,7 @@ import sys
 import os
 import pandas as pd
 
+def build_county_panel(start, end):
 
 
 if __name__ == '__main__':
@@ -9,7 +10,7 @@ if __name__ == '__main__':
     # Preliminaries
 
     ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-    DATA_PATH = os.path.join(ROOT_PATH, "drive\\raw_data\\qcew\\origin")
+    DATA_PATH = os.path.join(ROOT_PATH, "drive\\raw_data\\qcew")
 
     
 
