cad0b873a53f948372cd9324a92df035e51d4bda
Gabriele <gabriele_borg@brown.edu>
2020-06-24 09:16:13 -0400

    fix bug in make_demographics

--- base/demographics/code/make_demographics_ziplevel.R
+++ base/demographics/code/make_demographics_ziplevel.R
@@ -39,8 +39,44 @@ main <- function() {
    wgt <- 'tot_ratio'
    demovars <- setdiff(colnames(table_final), c(start_geo, target_geo, wgt, 'county_fips'))
    
-   table_final <- table_final[, lapply(.SD, weighted.mean, w = tot_ratio, na.rm = T), by = target_geo, .SDcols = demovars]
+   table_final <- table_final[, lapply(.SD, function(x, w) sum(x*w, na.rm = T), w=tot_ratio), by = target_geo, .SDcols = demovars]
    
+   
+   table_final[, c('urb_share2010', 
+                   'white_share2010', 'black_share2010', 'hisp_share2010', 'asian_share2010', 'natam_share2010', 
+                   'child_share2010', 'teen_share2010', 'youngadult_share2010', 'adult_share2010', 'elder_share2010', 
+                   'hh_couple_share2010', 'hh_couple_child_share2010',
+                   'renthouse_share2010',
+                   'work_county_share20105', 'worktravel_10_share20105', 'worktravel_10_60_share20105', 'worktravel_60_share20105', 
+                   'college_share20105', 'poor_share20105', 'lo_hhinc_share20105', 'hi_hhinc_share20105', 'unemp_share20105', 'employee_share20105') := list(
+                      (urb_share2010 / urb_share2010D), 
+                      (white_share2010 / race_share2010D),
+                      (black_share2010 / race_share2010D),
+                      (hisp_share2010 / race_share2010D),
+                      (asian_share2010 / race_share2010D),
+                      (natam_share2010 / race_share2010D),
+                      (child_share2010 / age_share2010D),
+                      (teen_share2010 / age_share2010D),
+                      (youngadult_share2010 / age_share2010D),
+                      (adult_share2010 / age_share2010D),
+                      (elder_share2010 / age_share2010D),
+                      (hh_couple_share2010 / hh_couple_share2010D), 
+                      (hh_couple_child_share2010 / hh_couple_child_share2010D),
+                      (renthouse_share2010 / renthouse_share2010D), 
+                      (work_county_share20105 / work_county_share20105D), 
+                      (worktravel_10_share20105 / worktravel_share_20105D),
+                      (worktravel_10_60_share20105 / worktravel_share_20105D),
+                      (worktravel_60_share20105 / worktravel_share_20105D),
+                      (college_share20105 / college_share20105D),
+                      (poor_share20105 / poor_share20105D), 
+                      (lo_hhinc_share20105 / hhinc_share20105D),
+                      (hi_hhinc_share20105 / hhinc_share20105D), 
+                      (unemp_share20105 / unemp_share20105D), 
+                      (employee_share20105 / employee_share20105D))]
+   
+   denom_cols <- colnames(table_final)[str_detect(colnames(table_final), "D$")]
+   
+   table_final[, (denom_cols):= NULL]
    
    save_data(table_final,
              filename = paste0(outdir, 'zip_demo.csv'),
@@ -73,38 +109,42 @@ format_tables <- function(x, datadir, data_version) {
       
       data[ , 'pop2010' := H7V001]
       
-      data[ , 'urb_share2010' := (H7W002 / H7W001)]
+      data[ , c('urb_share2010', 'urb_share2010D') := list(H7W002, H7W001)]
       
       data[, c('white_share2010', 
                'black_share2010', 
                'hisp_share2010', 
                'asian_share2010', 
-               'natam_share2010') := 
-              list((H7Z003 / H7Z001), 
-                   (H7Z004 / H7Z001), 
-                   (H7Z010 / H7Z001), 
-                   (H7Z006 / H7Z001), 
-                   (H7Z007 / H7Z001))]
+               'natam_share2010', 
+               'race_share2010D') := 
+              list(H7Z003, 
+                   H7Z004, 
+                   H7Z010, 
+                   H7Z006, 
+                   H7Z007, 
+                   H7Z001)]
       
       data[, c('child_share2010',      #0-14
                'teen_share2010',       #15-21
                'youngadult_share2010', #22-34
                'adult_share2010',      #35-64
-               'elder_share2010') :=   #65-
-              list((H76003 + H76004 + H76005 + H76027 + H76028 + H76029) / H76001, 
-                   (H76006 + H76007 + H76008 + H76009 + H76030 + H76031 + H76032 + H76033) / H76001, 
-                   (H76010 + H76011 + H76012 + H76034 + H76035 + H76036) / H76001, 
-                   (H76013 + H76014 + H76015 + H76016 + H76017 + H76018 + H76019 + H76037 + H76038 + H76039 + H76040 + H76041 + H76042 + H76043) / H76001,
-                   (H76020 + H76021 + H76022 + H76023 + H76024 + H76025 + H76044 + H76045 + H76046 + H76047 + H76048 + H76049) / H76001)]
+               'elder_share2010', 
+               'age_share2010D') :=   #65-
+              list((H76003 + H76004 + H76005 + H76027 + H76028 + H76029), 
+                   (H76006 + H76007 + H76008 + H76009 + H76030 + H76031 + H76032 + H76033), 
+                   (H76010 + H76011 + H76012 + H76034 + H76035 + H76036), 
+                   (H76013 + H76014 + H76015 + H76016 + H76017 + H76018 + H76019 + H76037 + H76038 + H76039 + H76040 + H76041 + H76042 + H76043),
+                   (H76020 + H76021 + H76022 + H76023 + H76024 + H76025 + H76044 + H76045 + H76046 + H76047 + H76048 + H76049), 
+                   H76001)]
       
       
       data[, 'housing_units2010':= IFC001]
       
       target_vars <- c('tract_fips', 'county_fips', 
                        'pop2010', 
-                       'urb_share2010', 
-                       'white_share2010', 'black_share2010', 'hisp_share2010', 'asian_share2010', 'natam_share2010', 
-                       'child_share2010', 'teen_share2010', 'youngadult_share2010', 'adult_share2010', 'elder_share2010', 
+                       'urb_share2010', 'urb_share2010D',
+                       'white_share2010', 'black_share2010', 'hisp_share2010', 'asian_share2010', 'natam_share2010', 'race_share2010D',
+                       'child_share2010', 'teen_share2010', 'youngadult_share2010', 'adult_share2010', 'elder_share2010', 'age_share2010D', 
                        'housing_units2010')
       
       
@@ -112,51 +152,53 @@ format_tables <- function(x, datadir, data_version) {
    
    else if (x == 'ds173_2010_tract.csv') {
       
-      data[, c('hh_couple_share2010',            #share of couples (married or partners) out of household population
-               'hh_couple_child_share2010') :=   #share of couples with children under 18 out of couple household population
-              list((IC6002 + IC6013) / IC6001, 
-                   (IC6004 + IC6009 + IC6015 + IC6020 + IC6025 + IC6030) / (IC6002 + IC6013))]
+      data[, c('hh_couple_share2010', 'hh_couple_share2010D',           #share of couples (married or partners) out of household population
+               'hh_couple_child_share2010', 'hh_couple_child_share2010D') :=   #share of couples with children under 18 out of couple household population
+              list((IC6002 + IC6013), IC6001, 
+                   (IC6004 + IC6009 + IC6015 + IC6020 + IC6025 + IC6030), (IC6002 + IC6013))]
     
       target_vars <- c('tract_fips', 'county_fips', 
-                       'hh_couple_share2010', 'hh_couple_child_share2010')
+                       'hh_couple_share2010', 'hh_couple_share2010D', 'hh_couple_child_share2010', 'hh_couple_child_share2010D')
       
       data <- data[ ,..target_vars]
    }
    
    else if (x == 'ds181_2010_tract.csv') {
-      data[, c('renthouse_share2010') :=  #share of housing units that are renter-occupied 
-              list(LHT004 / LHT001)]
+      data[, c('renthouse_share2010', 'renthouse_share2010D') :=  #share of housing units that are renter-occupied 
+              list(LHT004,LHT001)]
       
       target_vars <- c('tract_fips', 'county_fips', 
-                       'renthouse_share2010')
+                       'renthouse_share2010', 'renthouse_share2010D')
       
    }
    
    else if (x == 'ds191_20125_2012_tract.csv') {
       
-      data[, 'work_county_share20105' := QS6E003 / QS6E001]
+      data[, c('work_county_share20105', 'work_county_share20105D'):= list(QS6E003, QS6E001)]
       
       data[, c('worktravel_10_share20105',      #travel time to work less than 10 minutes (share of workers not at home)
                'worktravel_10_60_share20105',   #travel time to work between 10 and 60 minutes
-               'worktravel_60_share20105') :=   #travel time to work equal or above 60 minutes
-              list((QTHE002 + QTHE003) / QTHE001, 
-                   (QTHE004 + QTHE005 + QTHE006 + QTHE007 + QTHE008 + QTHE009 + QTHE010 + QTHE011) / QTHE001, 
-                   (QTHE012 + QTHE013) / QTHE001)]
+               'worktravel_60_share20105', 
+               'worktravel_share_20105D') :=   #travel time to work equal or above 60 minutes
+              list((QTHE002 + QTHE003), 
+                   (QTHE004 + QTHE005 + QTHE006 + QTHE007 + QTHE008 + QTHE009 + QTHE010 + QTHE011), 
+                   (QTHE012 + QTHE013), QTHE001)]
       
-      data[, 'college_share20105' := (QUSE021 + QUSE022 + QUSE023+ QUSE024 + QUSE025) / QUSE001]
+      data[, c('college_share20105', 'college_share20105D') := list((QUSE021 + QUSE022 + QUSE023+ QUSE024 + QUSE025), QUSE001)]
       
-      data[, 'poor_share20105' := (QUVE002 + QUVE003) / QUVE001]
+      data[, c('poor_share20105', 'poor_share20105D') := list((QUVE002 + QUVE003), QUVE001)]
       
       data[, c('lo_hhinc_share20105',       #share of households with income lower than 45k
-               'hi_hhinc_share20105') :=   #share of households with income higher or equal than 100k
-              list((QU0E002 + QU0E003 + QU0E004 + QU0E005 + QU0E006 + QU0E007 + QU0E008 + QU0E009) / QU0E001, 
-                   (QU0E014 + QU0E015 + QU0E016 + QU0E017) / QU0E001)]
+               'hi_hhinc_share20105', 
+               'hhinc_share20105D') :=   #share of households with income higher or equal than 100k
+              list((QU0E002 + QU0E003 + QU0E004 + QU0E005 + QU0E006 + QU0E007 + QU0E008 + QU0E009), 
+                   (QU0E014 + QU0E015 + QU0E016 + QU0E017), QU0E001)]
       
       data[, 'med_hhinc20105' := QU1E001]
       
-      data[, 'unemp_share20105' := QXSE005 / QXSE002]
+      data[, c('unemp_share20105', 'unemp_share20105D') := list(QXSE005, QXSE002)]
       
-      data[, 'employee_share20105' := (QX5E004 + QX5E014 + QX5E006 + QX5E016 + QX5E007 + QX5E008 + QX5E009 + QX5E017 + QX5E018 + QX5E019) / QX5E001]
+      data[, c('employee_share20105', 'employee_share20105D') := list((QX5E004 + QX5E014 + QX5E006 + QX5E016 + QX5E007 + QX5E008 + QX5E009 + QX5E017 + QX5E018 + QX5E019),QX5E001)]
       
       target_vars <- c('tract_fips', 'county_fips', 
                        'work_county_share20105', 
@@ -166,7 +208,8 @@ format_tables <- function(x, datadir, data_version) {
                        'lo_hhinc_share20105', 'hi_hhinc_share20105',
                        'med_hhinc20105',
                        'unemp_share20105', 
-                       'employee_share20105')
+                       'employee_share20105', 
+                       'work_county_share20105D', 'worktravel_share_20105D', 'college_share20105D', 'poor_share20105D', 'hhinc_share20105D', 'unemp_share20105D', 'employee_share20105D')
       
       
       
