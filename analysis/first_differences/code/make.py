6c2d118e4ef5165b75a4b2ac5f9db032d544f800
GBorg <30894427+gabrieleborg@users.noreply.github.com>
2020-10-13 07:45:44 -0400

    Update make.py

--- analysis/first_differences/code/make.py
+++ analysis/first_differences/code/make.py
@@ -35,7 +35,8 @@ elif "Stata15" in envir_vars:
 start_make_logging()
 
 run_stata(program = 'preclean.do', executable = stata)
-run_stata(program = 'first_differences.do', executable = stata)
+run_stata(program = 'estimate_base.do', executable = stata)
+run_stata(program = 'estimate_heteroegenity.do', executable = stata)
 run_stata(program = 'horse_race_dynamic_models.do', executable = stata)
 
 end_make_logging()
