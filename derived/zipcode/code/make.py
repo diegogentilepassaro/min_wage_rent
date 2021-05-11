568cd0f411b23b7afdc86cb26ea59a38a350f382
santiagohermo <santiagohermo@users.noreply.github.com>
2021-05-11 03:45:54 -0700

    Drop redundancy in names derived/zipcode and /zipcode_month

--- derived/zipcode/code/make.py
+++ derived/zipcode/code/make.py
@@ -34,7 +34,7 @@ elif "Stata15" in envir_vars:
 
 start_make_logging()
 
-run_stata(program = 'build_zipcode_panel.do', executable = stata)
+run_stata(program = 'build.do', executable = stata)
 
 end_make_logging()
 
