fc6b4b0bbf58198aecff585140d82407970db43d
Diego Rafael Gentile Passaro <diego.gentilepassaro@barcelonagse.eu>
2021-09-08 09:42:20 -0700

    #111 fill table within make.py framework

--- analysis/tables/code/fill_tables.py
+++
@@ -1,8 +0,0 @@
-import os
-from gslab_fill.tablefill import tablefill
-
-fd_baseline = "../../fd_baseline/output"
-
-tablefill(input    = fd_baseline + '/static.txt', 
-	      template = '../input/static.tex', 
-          output   = '../output/static.tex')
