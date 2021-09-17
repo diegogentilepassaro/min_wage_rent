4e472d370f1c2f8ee0fd68632d6c9e73d0dc806e
santiagohermo <santiagohermo@users.noreply.github.com>
2021-09-16 17:30:24 -0700

    Several updates to code for single events
    - Restructure code a bit
    - Increase dpi to 250
    - Also save figures in eps
    
    fyi @diegogentilepassaro

--- analysis/maps/code/plot_mw_rents_maps.R
+++ analysis/maps/code/plot_mw_rents_maps.R
@@ -1,7 +1,6 @@
 remove(list = ls())
 
 library(sf)
-library(data.table)
 library(dplyr)
 library(spData)
 library(tmap)
@@ -10,77 +9,86 @@ library(leaflet)
 library(ggplot2)
 
 main <- function(){
-  USPS_zipcodes <- read_sf(dsn = "../../../drive/raw_data/shapefiles/USPS_zipcodes", 
-                           layer = "USPS_zipcodes_July2020")
-  USPS_zipcodes <- USPS_zipcodes %>%
+  in_map  <- "../../../drive/raw_data/shapefiles/USPS_zipcodes"
+  in_data <- "../../../drive/derived_large/estimation_samples"
+  
+  df_all <- prepare_data(in_map, in_data)
+  
+  events <- list(list("san_diego", 41740, 2018, 12, 2019, 6),
+                 list("seattle",   42660, 2018, 12, 2019, 6),
+                 list("nyc",       35620, 2018, 12, 2019, 6),
+                 list("kc",        28140, 2018, 12, 2019, 6))
+                 # Name CBSA10, Code CBSA10, start date, end date
+  
+  lapply(events,
+    function(event) {
+      df <- restrict_and_build_changes(df_all, event[[2]],
+                                       event[[3]], event[[4]], event[[5]], event[[6]]) 
+      
+      build_map(df, "change_ln_actual_mw",
+                "Change in\nlog(MW)", paste0(event[[1]], "_actual_mw"))
+      build_map(df, "change_exp_ln_mw", 
+                "Change in\nexp. log(MW)", paste0(event[[1]], "_exp_mw"))
+      build_map(df, "change_ln_rents", 
+                "Change in\nlog(rents)", paste0(event[[1]], "_rents"))
+      
+    }
+  ) -> l
+}
+
+prepare_data <- function(in_map, in_data) {
+  
+  USPS_zipcodes <- read_sf(dsn = in_map, 
+                           layer = "USPS_zipcodes_July2020") %>%
     select(ZIP_CODE, PO_NAME, STATE, POPULATION, SQMI, POP_SQMI) %>%
     rename(zipcode = ZIP_CODE, zipcode_name = PO_NAME,
            state_name = STATE, pop2020 = POPULATION, 
            area_sq_miles = SQMI, pop2020_per_sq_miles = POP_SQMI)
   
-  mw_rent_data <- fread("../../../drive/derived_large/estimation_samples/all_zipcode_months.csv",
-                        colClasses = c(zipcode ="character", place_code ="character", 
-                                       countyfips = "character", cbsa10 = "character",
-                                       zcta ="character", place_name = "character", 
-                                       county_name = "character", cbsa10_name = "character",
-                                       state_abb = "character", statefips = "character")) %>%
+  mw_rent_data <- data.table::fread(file.path(in_data, "all_zipcode_months.csv"),
+                                    colClasses = c(zipcode ="character", place_code ="character", 
+                                                   countyfips = "character", cbsa10 = "character",
+                                                   zcta ="character", place_name = "character", 
+                                                   county_name = "character", cbsa10_name = "character",
+                                                   state_abb = "character", statefips = "character")) %>%
     select(zipcode, cbsa10, year, month, actual_mw, exp_ln_mw, medrentpricepsqft_SFCC) %>%
     mutate(ln_actual_mw = log(actual_mw),
            ln_rent_var = log(medrentpricepsqft_SFCC))
   
-  merged <- left_join(USPS_zipcodes, mw_rent_data, by = "zipcode")
-  
-  cbsa10_code_san_diego <- 41740
-  cbsa10_code_seattle <- 42660
-  cbsa10_code_nyc <- 35620
-  cbsa10_code_kc <- 28140
-  
-  san_diego <- restrict_and_build_changes(merged, cbsa10_code_san_diego, 
-                                          2018, 10, 
-                                          2019, 3)
-  seattle <- restrict_and_build_changes(merged, cbsa10_code_seattle, 
-                                          2018, 10, 
-                                          2019, 3)
-  nyc <- restrict_and_build_changes(merged, cbsa10_code_nyc, 
-                                          2018, 10, 
-                                          2019, 3)
-  kc <- restrict_and_build_changes(merged, cbsa10_code_kc, 
-                                          2018, 10, 
-                                          2019, 3)
-
-  for(cbsa in c("san_diego", "seattle", "nyc", "kc")){
-    build_map(get(cbsa), "change_ln_actual_mw", 
-              "Change in \n log(MW)", paste0(cbsa, "_ln_mw_change"))
-    build_map(get(cbsa), "change_exp_ln_mw", 
-              "Change in \n experienced log(MW)", paste0(cbsa, "_exp_ln_mw_change"))
-    build_map(get(cbsa), "change_ln_rents", 
-              "Change in \n log(MW)", paste0(cbsa, "_ln_rents_change"))
-  }
+  left_join(USPS_zipcodes, mw_rent_data, by = "zipcode")
 }
 
 restrict_and_build_changes <- function(data, cbsa10_code, year_lb, month_lb, 
                                        year_ub, month_ub){
-  restricted <- data %>%
+  data %>%
     filter(cbsa10 == cbsa10_code) %>%
-    filter((year == year_lb & month == month_lb) | (year == year_ub & month == month_ub)) %>%
+    filter(  (year == year_lb & month == month_lb) 
+           | (year == year_ub & month == month_ub)) %>%
     group_by(zipcode) %>%
-    summarise(change_actual_mw = last(actual_mw) - first(actual_mw), 
+    summarise(change_actual_mw    = last(actual_mw)   - first(actual_mw), 
               change_ln_actual_mw = last(ln_actual_mw) - first(ln_actual_mw),
-              change_exp_ln_mw = last(exp_ln_mw) - first(exp_ln_mw),
-              change_ln_rents = last(ln_rent_var) - first(ln_rent_var))
+              change_exp_ln_mw    = last(exp_ln_mw)   - first(exp_ln_mw),
+              change_ln_rents     = last(ln_rent_var) - first(ln_rent_var)) %>%
+    ungroup()
 }
 
-build_map <- function(data, var, var_legend, map_name){
+build_map <- function(data, var, var_legend, map_name,
+                      .dpi = 250){
+  
   map <- tm_shape(data) + 
     tm_fill(col = var,
             title = var_legend,
             style = "cont",
-            palette = c("white", "#077187")) +
-    tm_borders(lwd = .01, alpha = 0.5) +
+            palette = c("#A6E1F4", "#077187"),
+            textNA = "NA") +
+    tm_borders(col = "white", lwd = .01, alpha = 0.7) +
     tm_layout(legend.position = c("left", "bottom"))
+  
   tmap_save(map, 
             paste0("../output/", map_name, ".png"),
-            dpi = 50)  
+            dpi = .dpi)
+  tmap_save(map, 
+            paste0("../output/", map_name, ".eps"))
 }
 
 main()
