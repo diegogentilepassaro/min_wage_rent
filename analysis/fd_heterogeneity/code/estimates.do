df7d2fb3e8a1bcd777fdc2269ac7c57b1a59f35a
santiagohermo <santiagohermo@users.noreply.github.com>
2021-09-24 15:04:28 -0700

    Separate lodes od matrix and lodes area characteristics #119

--- analysis/fd_heterogeneity/code/estimates.do
+++ analysis/fd_heterogeneity/code/estimates.do
@@ -14,9 +14,35 @@ program main
 	
 	
 	** STATIC
-	use "`instub'/baseline_zipcode_months.dta", clear
-	xtset zipcode_num year_month
+	load_data, instub(`instub')
+	
+	reghdfe D.ln_med_rent_var d_ln_mw d_ln_mw_int d_exp_ln_mw d_exp_ln_mw_int, nocons absorb(year_month) cluster(statefips)
 
+	
+	
+end
+
+
+program load_data
+    syntax, instub(str)
+
+	use "`instub'/baseline_zipcode_months.dta", clear
+
+	bys statefips: egen share_resid_state_med = median(share_residents_lowinc)
+	bys statefips: egen share_work_state_med  = median(share_workers_lowinc)
+	
+	gen share_resid_above_med_st = (share_residents_lowinc > share_resid_state_med)
+	gen share_work_above_med_st  = (share_workers_lowinc   > share_work_state_med)
+
+	drop share_resid_state_med share_work_state_med
+	
+	xtset zipcode_num year_month
+	
+	gen d_ln_mw     = D.ln_mw
+	gen d_exp_ln_mw = D.exp_ln_mw
+	
+	gen d_ln_mw_int     = d_ln_mw*share_work_above_med_st
+	gen d_exp_ln_mw_int = d_exp_ln_mw*share_resid_above_med_st
 end
 
 
---
+++ base/lodes_area_charac/code/make_xwalk.R
@@ -0,0 +1,56 @@
+
+make_xwalk_od <- function(instub) {
+
+  xwalk_files <- list.files('../../../raw/crosswalk/lodes/', 
+                            full.names = T, pattern = "*.gz")
+  xwalk <- rbindlist(lapply(xwalk_files, function(x) fread(x)))
+
+  setnames(xwalk, old = c('tabblk2010', 'trct'), 
+                  new = c('blockfips', 'tract_fips'))
+  
+  xwalk[, tract_fips := as.numeric(tract_fips)]
+
+  xwalk <- xwalk[, c('blockfips', 'tract_fips', 'st')]
+  
+  tract_zip_xwalk <- fread(file.path(instub, "tract_zip_master.csv"), 
+                 colClasses = c('numeric', 'numeric', 'numeric'))
+  
+  return(list(xwalk, tract_zip_xwalk))
+}
+
+make_xwalk_raw_wac <- function(instub) {
+  xwalk_files <- list.files(file.path(instub, 'lodes'), full.names = T)
+
+  xwalk <- rbindlist(lapply(xwalk_files, function(x) fread(x)))
+  
+  setnames(xwalk, old = c('tabblk2010', 'trct'),
+           new = c('blockfips', 'tract_fips'))
+  target_xwalk <- c('blockfips', 'tract_fips', 'st')
+  xwalk[, tract_fips := as.numeric(tract_fips)]
+  xwalk <- xwalk[, ..target_xwalk]
+  
+  return(xwalk)
+}
+
+make_xwalk_raw_wac_county <- function(instub) {
+  xwalk_files <- list.files(file.path(instub, 'lodes'), full.names = T)
+  
+  xwalk <- rbindlist(lapply(xwalk_files, function(x) fread(x)))
+  
+  setnames(xwalk, old = c('tabblk2010', 'cty'), 
+           new = c('blockfips', 'countyfips'))
+
+  target_xwalk <- c('blockfips', 'countyfips', 'st')
+  xwalk[, countyfips := as.numeric(countyfips)]
+  xwalk <- xwalk[, ..target_xwalk]
+  
+  return(xwalk)
+}
+
+
+make_xwalk_tractzip <- function(instub) {
+  xwalk <- fread(file.path(instub, "tract_zip_master.csv"), 
+                 colClasses = c('numeric', 'numeric', 'numeric'))
+
+  return(xwalk)
+}
