f08560c272df6734fb132062c0199c022b6e4e42
santiagohermo <santiagohermo@users.noreply.github.com>
2021-10-15 16:58:20 -0700

    A fix to county shares
    fyi @diegogentilepassaro

--- derived/shares/code/build.R
+++ derived/shares/code/build.R
@@ -1,10 +1,8 @@
 remove(list = ls())
-options(scipen=999)
+library(data.table)
 
-source("../../../lib/R/load_packages.R")
 source("../../../lib/R/save_data.R")
 
-library("data.table")
 
 main <- function() {
   instub  <- "../../../drive/base_large/lodes_od"
@@ -13,15 +11,15 @@ main <- function() {
   for (geo in c("zip", "county")) {
     
     if (geo == "zip") {
-      patt  = "odzip_*"
-      h_geo = "r_zipcode"
-      w_geo = "w_zipcode"
-      key_  = "zipcode"
+      patt     = "odzip_*"
+      h_geo    = "r_zipcode"
+      w_geo    = "w_zipcode"
+      geo_name = "zipcode"
     } else {
       patt = "odcounty*"
-      h_geo = "r_countyfips"
-      w_geo = "w_countyfips"
-      key_  = "county"
+      h_geo     = "r_countyfips"
+      w_geo     = "w_countyfips"
+      geo_name  = "county"
     }
     
     dt <- data.table()
@@ -34,25 +32,25 @@ main <- function() {
                                      colClasses = c(h_geo = "character",
                                                     w_geo = "character")))
       
-      dt.year <- group_matrix_sum(od_matrix, key_, h_geo, w_geo)
+      dt.year <- group_matrix_sum(od_matrix, geo_name, h_geo, w_geo)
       
       dt.year <- compute_shares(dt.year)
       dt.year[, year := yy]
-      dt.year <- dt.year[!duplicated(dt.year$zipcode)]
+      dt.year <- dt.year[!duplicated(dt.year[, ..geo_name])]
       
       dt <- rbindlist(list(dt, dt.year))
     }
     
-    save_data(dt, key = c(key_, "year"),
-              filename = file.path(outstub, paste0(key_, "_shares.csv")),
+    save_data(dt, key = c(geo_name, "year"),
+              filename = file.path(outstub, paste0(geo_name, "_shares.csv")),
               logfile  = "../output/shares_data_manifest.log")
   }
 }
 
 add_missing_state_years <- function(od_files, instub, geo, yy) {
   
-  if (geo == "countyfips") geo <- "county"
-  else                     geo <- "zip"
+  if (geo == "county") geo <- "county"
+  else                 geo <- "zip"
   
   if (yy == 2009) {
     return(c(od_files, sprintf("%s/2010/od%s_11.csv", instub, geo), 
@@ -66,38 +64,37 @@ add_missing_state_years <- function(od_files, instub, geo, yy) {
   }
 }
 
-group_matrix_sum <- function(od, key_, h_geo, w_geo) {
+group_matrix_sum <- function(od, geo_name, h_geo, w_geo) {
   
   residents <- od[, .(residents        = sum(jobs_tot), 
                       residents_young  = sum(jobs_age_under29),
                       residents_lowinc = sum(jobs_earn_under1250)),
                   by = h_geo]
-  setnames(residents, h_geo, key_)
+  setnames(residents, h_geo, geo_name)
   
   
   workers <- od[, .(workers        = sum(jobs_tot), 
                     workers_young  = sum(jobs_age_under29),
                     workers_lowinc = sum(jobs_earn_under1250)),
                 by = w_geo]
-  setnames(workers, w_geo, key_)
+  setnames(workers, w_geo, geo_name)
   
   workers_own_zip <- od[get(h_geo) == get(w_geo)]
-  vars <- c(h_geo, w_geo, "jobs_tot", "jobs_age_under29", "jobs_earn_under1250")
+  vars <- c(h_geo, "jobs_tot", "jobs_age_under29", "jobs_earn_under1250")
   workers_own_zip <- workers_own_zip[, ..vars]
   
-  setnames(workers_own_zip, old = c(h_geo, "jobs_tot",   "jobs_age_under29",      "jobs_earn_under1250"),
-           new = c(key_,  "own_jobs", "own_jobs_young", "own_jobs_lowinc"))
+  setnames(workers_own_zip, old = c(h_geo,    "jobs_tot", "jobs_age_under29", "jobs_earn_under1250"),
+                            new = c(geo_name, "own_jobs", "own_jobs_young",   "own_jobs_lowinc"))
   
-  dt <- merge(workers, residents, 
-              all.x = T, by = key_)
-  dt <- merge(dt, workers_own_zip, 
-              all.x = T, by = key_)
+  dt <- merge(workers, residents,
+              all.x = T, by = geo_name)
+  dt <- merge(dt, workers_own_zip,
+              all.x = T, by = geo_name)
   
   return(dt)
 }
 
 compute_shares <- function(dt) {
-  
   
   dt[, share_residents_young  := residents_young/residents]
   dt[, share_residents_lowinc := residents_lowinc/residents]
