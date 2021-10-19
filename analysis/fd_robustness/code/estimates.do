35a8bce7cc2238a0c2e1a01b4854aa8a5f15de60
Diego Rafael Gentile Passaro <diego_gentile_passaro@brown.edu>
2021-10-18 19:46:08 -0700

    PR for #113: update estimations and create robustness tables (#137)
    * Add static_sample table #113
    
    * Flip order of stat_var and dyn_var #113
    
    Aesthetic change
    
    * Add template for static model with Arellano-Bond #113
    
    * create estimates results and format static tables
    
    * updated dynamic plot comparison
    
    * #115 added extra cols, renamed tables
    
    * #113 Ditch dynamic robustness and build new tables
    
    * #113 Build robustness tables and fill them (some filling is not working)
    
    * #113 Update robustness tables
    
    * Minor fixes to code in `fd_robustness`
    
    * Simplifying "tables" code #113
    
    * Re-runs robustness making sure everything works
    
    * Simplifies names of models and swaps column in static_sample
    
    * Haciendo andar las tablas (no usar tabulars adentro de tabulars!)
    
    * Changing models with geographies
    
    * Makes sure all tables work after updating from master
    
    * Make sure everything works step 2
    
    * Delete figures.log
    
    Co-authored-by: Santiago Hermo <45404755+santiagohermo@users.noreply.github.com>
    Co-authored-by: santiagohermo <santiagohermo@users.noreply.github.com>
    Co-authored-by: GBorg <30894427+gabrieleborg@users.noreply.github.com>

---
+++ analysis/fd_robustness/code/estimates.do
@@ -0,0 +1,105 @@
+clear all
+set more off
+adopath + ../../../lib/stata/gslab_misc/ado
+adopath + ../../../lib/stata/min_wage/ado
+set maxvar 32000 
+
+program main
+    local instub "../../../drive/derived_large/estimation_samples"
+    local outstub "../output"
+    
+    define_controls
+    local controls "`r(economic_controls)'"    
+    local cluster  "statefips"
+    
+    
+    use "`instub'/baseline_zipcode_months.dta", clear
+    xtset zipcode_num year_month
+
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(`controls') absorb(year_month) cluster(`cluster') ///
+        model_name(static_baseline) outfolder("../temp")
+        
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(" ") absorb(year_month) cluster(`cluster') ///
+        model_name(static_nocontrols) outfolder("../temp")
+
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(`controls') ab absorb(year_month) cluster(`cluster') ///
+        model_name(static_AB) outfolder("../temp")
+
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality wgt(wgt_cbsa100) ///
+        controls(`controls') absorb(year_month) cluster(`cluster') ///
+        model_name(static_baseline_wgt) outfolder("../temp")
+        
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(`controls') absorb(year_month zipcode) cluster(`cluster') ///
+        model_name(static_zip_spec_trend) outfolder("../temp")
+        
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(`controls') absorb(year_month##county_num) ///
+        cluster(`cluster') ///
+        model_name(static_county_timefe) outfolder("../temp")
+
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(`controls') absorb(year_month##cbsa10_num) ///
+        cluster(`cluster') ///
+        model_name(static_cbsa_timefe) outfolder("../temp")
+        
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(`controls') absorb(year_month##statefips_num) ///
+        cluster(`cluster') ///
+        model_name(static_state_timefe) outfolder("../temp")
+    
+    use "`instub'/all_zipcode_months.dta", clear
+    xtset zipcode_num year_month
+
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(`controls') absorb(year_month) cluster(`cluster') ///
+        model_name(static_unbal) outfolder("../temp")
+    
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality wgt(wgt_cbsa100) ///
+        controls(`controls') absorb(year_month) cluster(`cluster') ///
+        model_name(static_unbal_wgt) outfolder("../temp")
+
+    
+    use "`instub'/balanced_zipcode_months.dta", clear
+    xtset zipcode_num year_month    
+    
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality ///
+        controls(`controls') absorb(year_month) cluster(`cluster') ///
+        model_name(static_fullbal) outfolder("../temp")
+    
+    estimate_dist_lag_model, depvar(ln_rents) ///
+        dyn_var(exp_ln_mw_17) w(0) stat_var(ln_mw) test_equality wgt(wgt_cbsa100) ///
+        controls(`controls') absorb(year_month) cluster(`cluster') ///
+        model_name(static_fullbal_wgt) outfolder("../temp")
+        
+
+    use "../temp/estimates_static_baseline.dta", clear
+    foreach ff in static_nocontrols          static_AB                 ///
+                  static_zip_spec_trend      static_state_timefe       ///
+                  static_county_timefe       static_cbsa_timefe        ///
+                  static_unbal               static_unbal_wgt          ///
+                  static_fullbal             static_fullbal_wgt        ///
+                  static_baseline_wgt {
+        
+        append using ../temp/estimates_`ff'.dta
+    }
+    save             `outstub'/estimates_static.dta, replace
+    export delimited `outstub'/estimates_static.csv, replace
+end
+
+
+main
---
+++ analysis/fd_robustness/code/tables.R
@@ -0,0 +1,113 @@
+remove(list = ls())
+library(data.table)
+
+main <- function() {
+  instub  <- "../output/"
+  outstub <- "../output/"
+  
+  est <- fread(file.path(instub, "estimates_static.csv"))
+
+
+  txt_static_sample <- c("<tab:static_sample>")
+  for (xvar in c("ln_mw", "exp_ln_mw_17", "cumsum_from0")) {
+    txt_static_sample <- c(txt_static_sample, 
+                            paste(est[model == "static_baseline"     & var == xvar,]$b,
+                                  est[model == "static_baseline_wgt" & var == xvar,]$b,
+                                  est[model == "static_unbal"        & var == xvar,]$b,
+                                  est[model == "static_unbal_wgt"    & var == xvar,]$b,
+                                  est[model == "static_fullbal"      & var == xvar,]$b,
+                                  est[model == "static_fullbal_wgt"  & var == xvar,]$b,
+                                  sep = "\t"))
+    txt_static_sample <- c(txt_static_sample, 
+                            paste(est[model == "static_baseline"     & var == xvar,]$se,
+                                  est[model == "static_baseline_wgt" & var == xvar,]$se,
+                                  est[model == "static_unbal"        & var == xvar,]$se,
+                                  est[model == "static_unbal_wgt"    & var == xvar,]$se,
+                                  est[model == "static_fullbal"      & var == xvar,]$se,
+                                  est[model == "static_fullbal_wgt"  & var == xvar,]$se,
+                                  sep = "\t"))
+  }
+
+  for (stat in c("p_equality", "r2", "N")) {
+    txt_static_sample <- c(txt_static_sample, 
+                            paste(est[model == "static_baseline"     & var == "cumsum_from0",][[stat]],
+                                  est[model == "static_baseline_wgt" & var == "cumsum_from0",][[stat]],
+                                  est[model == "static_unbal"        & var == "cumsum_from0",][[stat]],
+                                  est[model == "static_unbal_wgt"    & var == "cumsum_from0",][[stat]],
+                                  est[model == "static_fullbal"      & var == "cumsum_from0",][[stat]],
+                                  est[model == "static_fullbal_wgt"  & var == "cumsum_from0",][[stat]],
+                                  sep = "\t"))
+  }
+
+  fileConn <- file(file.path(outstub, "static_sample.txt"))
+  writeLines(txt_static_sample, fileConn)
+  close(fileConn)
+  
+
+  txt_static_ab <- c("<tab:static_ab>")
+  for (xvar in c("ln_mw", "exp_ln_mw_17")) {
+    txt_static_ab <- c(txt_static_ab, 
+                      paste(est[model == "static_baseline" & var == xvar,]$b,
+                            est[model == "static_AB"       & var == xvar,]$b,
+                            sep = "\t"))
+    txt_static_ab <- c(txt_static_ab, 
+                      paste(est[model == "static_baseline" & var == xvar,]$se,
+                            est[model == "static_AB"       & var == xvar,]$se,
+                            sep = "\t"))
+  }
+
+  txt_static_ab <- c(txt_static_ab,
+                  paste(est[model == "static_AB" & var == "L_ln_rents",]$b,
+                        sep = "\t"))
+  txt_static_ab <- c(txt_static_ab,
+                  paste(est[model == "static_AB" & var == "L_ln_rents",]$se,
+                        sep = "\t"))
+  
+  for (stat in c("p_equality", "r2", "N")) {
+    txt_static_ab <- c(txt_static_ab, 
+                        paste(est[model == "static_baseline" & var == "exp_ln_mw_17",][[stat]],
+                              est[model == "static_AB"       & var == "exp_ln_mw_17",][[stat]],
+                              sep = "\t"))
+  }
+
+  fileConn <- file(file.path(outstub, "static_ab.txt"))
+  writeLines(txt_static_ab, fileConn)
+  close(fileConn) 
+
+
+  txt_static_robust <- c("<tab:static_robust>")
+  for (xvar in c("ln_mw", "exp_ln_mw_17", "cumsum_from0")) {
+    txt_static_robust <- c(txt_static_robust,
+                           paste(est[model == "static_baseline"        & var == xvar,]$b,
+                                 est[model == "static_nocontrols"      & var == xvar,]$b,
+                                 est[model == "static_zip_spec_trend"  & var == xvar,]$b,
+                                 est[model == "static_county_timefe"   & var == xvar,]$b,
+                                 est[model == "static_cbsa_timefe"     & var == xvar,]$b,
+                                 est[model == "static_state_timefe"    & var == xvar,]$b,
+                                 sep = "\t"))
+    txt_static_robust <- c(txt_static_robust,
+                           paste(est[model == "static_baseline"        & var == xvar,]$se,
+                                 est[model == "static_nocontrols"      & var == xvar,]$se,
+                                 est[model == "static_zip_spec_trend"  & var == xvar,]$se,
+                                 est[model == "static_county_timefe"   & var == xvar,]$se,
+                                 est[model == "static_cbsa_timefe"     & var == xvar,]$se,
+                                 est[model == "static_state_timefe"    & var == xvar,]$se,
+                                 sep = "\t"))
+  }
+  for (stat in c("p_equality", "r2", "N")) {
+    txt_static_robust <- c(txt_static_robust, 
+                          paste(est[model == "static_baseline"       & var == "exp_ln_mw_17",][[stat]],
+                                est[model == "static_nocontrols"     & var == "exp_ln_mw_17",][[stat]],
+                                est[model == "static_zip_spec_trend" & var == "exp_ln_mw_17",][[stat]],
+                                est[model == "static_county_timefe"  & var == "exp_ln_mw_17",][[stat]],
+                                est[model == "static_cbsa_timefe"    & var == "exp_ln_mw_17",][[stat]],
+                                est[model == "static_state_timefe"   & var == "exp_ln_mw_17",][[stat]],
+                                sep = "\t"))
+  }
+  fileConn <- file(file.path(outstub, "static_robust.txt"))
+  writeLines(txt_static_robust, fileConn)
+  close(fileConn)
+}
+
+
+main()
