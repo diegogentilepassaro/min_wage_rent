3b546d52c7085ffc50d46bc85ee5f92270c87c8d
Martín Gallardo <69550249+martingallardo23@users.noreply.github.com>
2022-02-15 01:17:32 -0300

    Make some changes #160

--- base/hud_housing_assistance/code/build.R
+++ base/hud_housing_assistance/code/build.R
@@ -2,61 +2,27 @@ remove(list = ls())
 
 library(data.table)
 library(stringr)
-library(magrittr)
 
 source("../../../lib/R/save_data.R")
 
 main <- function () {
-  vars <-
-    c(
-      "total_units",
-      "pct_occupied",
-      "number_reported",
-      "pct_reported",
-      "months_since_report",
-      "people_per_unit",
-      "people_total",
-      "rent_per_month",
-      "spending_per_month",
-      "hh_income",
-      "person_income",
-      "pct_wage_major",
-      "pct_lt50_median",
-      "pct_2adults",
-      "pct_1adult",
-      "pct_minority",
-      "pct_black_nonhsp",
-      "pct_hispanic",
-      "pct_bed1",
-      "pct_bed2",
-      "pct_bed3",
-      "tpoverty",
-      "year",
-      "zipcode",
-      "census_tract",
-      "city",
-      "program_label"
-    )
+  in_hud <- '../../../drive/raw_data/hud_housing_assistance'
+  outdir <- '../../../drive/base_large/hud_housing_assistance'
+  
+  vars <- fread('variables.csv', header = F)$V1
   
   geographies <- c("zipcode", "census_tract")
   
-  data_path <- '../../../drive/raw_data/hud_housing_assistance'
-  
-  drive_path <- '../../../drive/base_large/hud_housing_assistance'
-  
   for (gg in geographies) {
-    files_path <- file.path(data_path, gg)
+    files_path <- file.path(in_hud, gg)
     files_names <-
       list.files(files_path, pattern = ".xlsx", full.names = T)
     
     data <- rbindlist(lapply(files_names, function (x) {
-      year <- str_remove(x, ".*/") %>%
-        str_remove("_2010geography.*") %>%
-        str_remove("TRACT_AK_MN_") %>%
-        str_remove("TRACT_MO_WY_") %>%
-        str_remove("Zipcode_") %>%
-        str_remove(".xlsx") %>%
-        as.numeric()
+      if (gg == "zipcode")
+        year <- as.integer(substr(str_remove(x, ".*/"), 9, 12))
+      if (gg == "census_tract")
+        year <- as.integer(substr(str_remove(x, ".*/"), 13, 16))
       
       data <- as.data.table(readxl::read_xlsx(x))[, year := year]
       
@@ -64,40 +30,37 @@ main <- function () {
       
       vars_av <- intersect(names(data), vars)
       
+      data <- manual_fixes(data, gg, year)
+      
       return(data[, ..vars_av])
     }),
     fill = TRUE)
     
-    # Remove duplicated observations
-    
-    if (gg == "census_tract") {
-      data <- data[!(
-        program_label == "Summary of All HUD Programs" &
-          census_tract == "29186960200" &
-          year == 2015 &
-          total_units == 9
-      ) &
-        !(
-          program_label == "Project Based Section 8" &
-            census_tract == "29186960200" &
-            year == 2015 &
-            total_units == 9
-        )]
-    }
-    
     save_data(
       data,
       key = c(gg, "program_label", "year"),
-      filename = file.path(drive_path, paste0(gg, ".csv")),
-      logfile = file.path("../output", paste0(gg, ".log"))
+      filename = file.path(outdir, paste0(gg, ".csv")),
+      logfile = file.path("../output", paste0(gg, "_data_manifest.log"))
     )
     save_data(
       data,
       key = c(gg, "program_label", "year"),
-      filename = file.path(drive_path, paste0(gg, ".dta")),
+      filename = file.path(outdir, paste0(gg, ".dta")),
       nolog = TRUE
     )
   }
 }
 
+manual_fixes <- function(data, gg, year) {
+  if (gg == "census_tract" & year == 2015) {
+    data <-
+      data[entities != "MO Missouri 186 Ste. Genevieve County 29186960200"]
+  }
+  data <- as.data.table(lapply(data, function(x)
+    ifelse(x == -1, NA, x)))
+  
+  return(data)
+}
+
+# Execute
 main()
