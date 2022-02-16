6817f24c51ae05adf0de2a1b2fec9a7d272f74c3
santiagohermo <santiagohermo@users.noreply.github.com>
2022-02-15 17:41:18 -0800

    Style for compute_centroids #161

--- base/census_blocks_centroids/code/compute_centroids.R
+++ base/census_blocks_centroids/code/compute_centroids.R
@@ -5,22 +5,24 @@ library(dplyr)
 library(data.table)
 
 main <- function(){
-  in_stub <- "../temp"
-  out_stub <- "../../../drive/base_large/census_blocks_centroids"
+  instub  <- "../temp"
+  outstub <- "../../../drive/base_large/census_blocks_centroids"
   
-  
-  files <- list.files(path = in_stub, pattern = "*.shp$")
+  files <- list.files(path = instub, pattern = "*.shp$")
 
   cb_centroids <- data.frame()
   for (file in files){
     cb_file_centroids <- compute_centroids(in_stub, file)
-    cb_centroids <- rbind(cb_centroids, cb_file_centroids)
+    cb_centroids      <- rbind(cb_centroids, cb_file_centroids)
   }
-  st_write(cb_centroids, paste0(out_stub, "/census_blocks_2010_centroids.shp"))
+
+  st_write(cb_centroids, 
+           file.path(outstub, "census_blocks_2010_centroids.shp"))
 }
 
-compute_centroids <- function(in_stub, file){
-  spf <- read_sf(paste0(in_stub, "/", file)) %>%
+compute_centroids <- function(instub, file){
+
+  spf <- read_sf(file.path(instub, file)) %>%
     select(STATEFP10, COUNTYFP10, TRACTCE10,
            BLOCKID10, HOUSING10, POP10) %>%
     rename(statefips = STATEFP10,
@@ -29,10 +31,12 @@ compute_centroids <- function(in_stub, file){
            census_block = BLOCKID10, 
            num_houses10 = HOUSING10,
            pop10 = POP10)
-  spf <- spf[st_is_valid(spf),]
+
+  spf           <- spf[st_is_valid(spf),]
   spf_centroids <- st_centroid(spf)
 
   return(spf_centroids)
 }
+
 
 main()
