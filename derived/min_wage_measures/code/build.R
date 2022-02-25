c099de73d61f533fd54e45f35886ddf6d1cfcb92
santiagohermo <santiagohermo@users.noreply.github.com>
2022-02-24 19:59:23 -0800

    Changing approach to compute workplace MW and solve duplicated zip codes issue #162

--- derived/min_wage_measures/code/build.R
+++ derived/min_wage_measures/code/build.R
@@ -6,11 +6,12 @@ paquetes <- c("data.table", "zoo")
 lapply(paquetes, require, character.only = TRUE)
 
 library(parallel)
-n_cores <- 16
+n_cores <- 20
 
 main <- function(paquetes, n_cores){
   in_mw    <- "../../../drive/derived_large/min_wage_panels"
   in_lodes <- "../../../drive/base_large/lodes_od"
+  in_geo   <- "../../../drive/base_large/census_block_master"
   outstub  <- "../../../drive/derived_large/min_wage_measures"
   log_file <- "../output/data_file_manifest.log"
   
@@ -33,11 +34,48 @@ main <- function(paquetes, n_cores){
     
     for (yy in c(2017)) { #2009:2018
       
+      # Preliminaries
+      mw_var = "statutory_mw"
+      
+      if (geo == "countyfips") {
+        .w_var = "w_countyfips"
+        .r_var = "r_countyfips"
+      } else {
+        .w_var = "w_zipcode"
+        .r_var = "r_zipcode"
+      }
       od_files <- list.files(file.path(in_lodes, yy), 
                              pattern = sprintf("od%s*", substr(geo, 1, 3)),
                              full.names = T)
       
       od_files <- add_missing_state_years(od_files, in_lodes, geo, yy)
+      
+      
+      # Load od matrix
+      dt_od <- rbindlist(
+        lapply(od_files, function(ff) load_od_matrix(ff, geo, .w_var, .r_var))
+      )
+      dt_od <- dt_od[get(paste0("r_", geo)) != ""]           # Why are there zip codes with empty string as name?
+      dt_od <- dt_od[get(paste0("w_", geo)) != ""]           # Why are there zip codes with empty string as name?
+      
+      jobs_vars <- names(dt_od)[grepl("jobs", names(dt_od))]
+      
+      dt_od <- dt_od[, lapply(.SD, sum),                     # Group zip codes that appear in multiple states
+                     by = c(.w_var, .r_var),
+                     .SDcols = jobs_vars]
+      
+      dt_od[, c(paste0("r_", jobs_vars)) := lapply(.SD, sum, na.rm = T) , # Sum all jobs originating in residence zip codes
+         .SDcols = jobs_vars, 
+         by = c(.r_var)]
+      
+      for (var in jobs_vars) {                                        # Compute share of job to each destination for each jobs_var
+        dt_od[, c(gsub("jobs_", "sh_", var)) := get(var)/get(paste0("r_", var))]
+        dt_od[, c(var, paste0("r_", var))    := NULL]
+      }
+      
+      jobs_vars <- names(dt_od)[grepl("sh", names(dt_od))]
+      
+      setorderv(dt_od, c(.r_var, .w_var))
       
       # Parallel set-up
       cl <- makeCluster(n_cores, type = "PSOCK")   # Create cluster. Use type = "FORK" in Mac
@@ -45,16 +83,43 @@ main <- function(paquetes, n_cores){
       clusterExport(cl, "paquetes")                                         # Load "paquetes" object in nodes
       clusterEvalQ(cl, lapply(paquetes, require, character.only = TRUE))    # Load packages in nodes
       clusterExport(cl, "load_od_matrix", env = .GlobalEnv)                 # Load global environment objects in nodes
-      clusterExport(cl, "assemble_mw_wkp_state", env = .GlobalEnv)           # Load global environment objects in nodes
-      clusterExport(cl, c("dt", "periods", "in_mw", "in_lodes", "outstub", "geo"), 
+      clusterExport(cl, c("dt", "periods", "in_mw", "in_lodes", "outstub", 
+                          "geo", ".w_var", ".r_var", "dt_od", "mw_var", "jobs_vars"), 
                     env = environment())                                    # Load local environment objects in nodes
       
-      # Build wkp MW
-      dt_mw <- parLapply(cl, od_files, function(ff) {
-        
-        dt_st <- assemble_mw_wkp_state(ff, yy, periods, "statutory_mw", dt, in_lodes, geo)
-        return(dt_st)
-      })
+      # Build wkp MW for each period
+      dt_mw <- parLapply(cl, periods, function(ym, odm = dt_od, dt_geo = dt,
+                                               .geo = geo, w_var = .w_var, h_var = .r_var) {
+          
+          dt_ym <- dt_geo[year_month == ym, ]            # Select given date
+          dt_ym[, c(w_var) := get(.geo)]                  # Create matching variable
+          
+          vars_to_keep <- c(w_var, mw_var, "year_month")
+          dt_ym <- dt_ym[, ..vars_to_keep]
+          
+          dt_ym <- dt_ym[odm, on = w_var]         # Paste MW to every residence(h)-workplace(w) combination in 'dt_od'
+          dt_ym <- dt_ym[!is.na(year_month),]     # Drop missings (geo not showing up in mw data)
+          
+          dt_ym <- dt_ym[,
+                         .(mw_wkp_tot            = sum(log(get(mw_var))*sh_tot,               na.rm = T),
+                           mw_wkp_age_under29    = sum(log(get(mw_var))*sh_age_under29,       na.rm = T),
+                           mw_wkp_age_30to54     = sum(log(get(mw_var))*sh_age_30to54,        na.rm = T),
+                           mw_wkp_age_above55    = sum(log(get(mw_var))*sh_age_above55,       na.rm = T),
+                           mw_wkp_earn_under1250 = sum(log(get(mw_var))*sh_earn_under1250,    na.rm = T),
+                           mw_wkp_earn_1250_3333 = sum(log(get(mw_var))*sh_earn_1250_3333,    na.rm = T),
+                           mw_wkp_earn_above3333 = sum(log(get(mw_var))*sh_earn_above3333,    na.rm = T),
+                           mw_wkp_goods_prod     = sum(log(get(mw_var))*sh_goods_producing,   na.rm = T),
+                           mw_wkp_trad_tran_util = sum(log(get(mw_var))*sh_trade_transp_util, na.rm = T),
+                           mw_wkp_other_serv_ind = sum(log(get(mw_var))*sh_other_service_industry, na.rm = T)),
+                         .SDcols = jobs_vars,
+                         by = c(h_var, "year_month")
+          ]
+          
+          setnames(dt_ym, old = h_var, new = .geo)
+          
+          return(dt_ym)
+        })
+      stopCluster(cl)
       
       dt_mw <- rbindlist(dt_mw)
       
@@ -103,55 +168,6 @@ add_missing_state_years <- function(od_files, instub, geo, yy) {
   }
 }
 
-assemble_mw_wkp_state <- function(ff, yy, periods, mw_var, dt, in_lodes, .geo) {
-  
-  if (.geo == "countyfips") {
-    .w_var = "w_countyfips"
-    .r_var = "r_countyfips"
-  }
-  else {
-    .w_var = "w_zipcode"
-    .r_var = "r_zipcode"
-  }
-  
-  dt_od     <- load_od_matrix(ff, .geo, .w_var, .r_var)
-  jobs_vars <- names(dt_od)[grepl("jobs", names(dt_od))]
-  
-  # Computes share of treated and workplace MW for every period
-  dts.period <- lapply(periods, function(ym, od.st = dt_od, dt_geo = dt,
-                                         geo = .geo, w_var = .w_var, h_var = .r_var) {
-     
-     dt_ym <- dt_geo[year_month == ym, ]            # Select given date
-     dt_ym[, c(w_var) := get(geo)]                  # Create matching variable
-     
-     vars_to_keep <- c(w_var, mw_var, "year_month")
-     dt_ym <- dt_ym[, ..vars_to_keep]
-     
-     dt_ym <- dt_ym[od.st, on = w_var]       # Paste MW to every residence(h)-workplace(w) combination in 'dt_od'
-     dt_ym <- dt_ym[!is.na(year_month),]     # Drop missings (geo not showing up in mw data)
-   
-     dt_ym <- dt_ym[,
-         .(mw_wkp_tot            = sum(log(get(mw_var))*sh_tot,               na.rm = T),
-           mw_wkp_age_under29    = sum(log(get(mw_var))*sh_age_under29,       na.rm = T),
-           mw_wkp_age_30to54     = sum(log(get(mw_var))*sh_age_30to54,        na.rm = T),
-           mw_wkp_age_above55    = sum(log(get(mw_var))*sh_age_above55,       na.rm = T),
-           mw_wkp_earn_under1250 = sum(log(get(mw_var))*sh_earn_under1250,    na.rm = T),
-           mw_wkp_earn_1250_3333 = sum(log(get(mw_var))*sh_earn_1250_3333,    na.rm = T),
-           mw_wkp_earn_above3333 = sum(log(get(mw_var))*sh_earn_above3333,    na.rm = T),
-           mw_wkp_goods_prod     = sum(log(get(mw_var))*sh_goods_producing,   na.rm = T),
-           mw_wkp_trad_tran_util = sum(log(get(mw_var))*sh_trade_transp_util, na.rm = T),
-           mw_wkp_other_serv_ind = sum(log(get(mw_var))*sh_other_service_industry, na.rm = T)),
-      .SDcols = jobs_vars,
-      by = c(h_var, "year_month")
-    ]
-     
-    setnames(dt_ym, old = h_var, new = geo)
-   
-    return(dt_ym)
-  })
-  
-  return(rbindlist(dts.period))
-}
 
 load_od_matrix <- function(ff, geo, workplace_var, residence_var) {
   
@@ -159,27 +175,12 @@ load_od_matrix <- function(ff, geo, workplace_var, residence_var) {
     od <- fread(file.path(ff),
                 colClasses = c("r_countyfips" = "character",
                                "w_countyfips" = "character"))
-  } else{
+  } else {
     od <- fread(file.path(ff), 
                 colClasses = c("r_zipcode" = "character",
                                "w_zipcode" = "character"))
   }
   
-  
-  jobs_vars <- names(od)[grepl("jobs", names(od))]
-  
-  # Sum all jobs originitaing in residence zipcode
-  od[, c(paste0("r_", jobs_vars)) := lapply(.SD, sum, na.rm = T) , 
-    .SDcols = jobs_vars, 
-    by = residence_var]
-  
-  # Compute share of job to each destination for each jobs_var
-  for (var in jobs_vars) {
-    od[, c(gsub("jobs_", "sh_", var)) := get(var)/get(paste0("r_", var))]
-    od[, c(var, paste0("r_", var))    := NULL]
-  }
-  
-  setorderv(od, c(workplace_var, residence_var))
   return(od)
 }
 
