4c88d67ad2819f76a6d4be38efc4d9e5daa5fd28
santiagohermo <santiagohermo@users.noreply.github.com>
2022-03-05 07:00:23 -0800

    build_block_demo --> build_block_census

--- derived/demographics_at_baseline/code/make.py
+++ derived/demographics_at_baseline/code/make.py
@@ -32,7 +32,7 @@ if "StataSE" in envir_vars:
 
 start_make_logging()
 
-run_rbatch(program = 'build_block_demo.R')
+run_rbatch(program = 'build_block_census.R')
 run_rbatch(program = 'build_tract_acs.R')
 run_stata(program  = 'build_tract_mw_shares.do', executable = stata)
 run_rbatch(program = 'build_zip_demo.R')
