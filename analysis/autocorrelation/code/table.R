c819892fa83023f0c536eb6666a077eaad2d00fd
Martín Gallardo <69550249+martingallardo23@users.noreply.github.com>
2022-03-18 21:33:39 -0300

    Fix and add .txt #180

---
+++ analysis/autocorrelation/code/table.R
@@ -0,0 +1,37 @@
+remove(list = ls())
+
+library(data.table)
+
+main <- function() {
+  instub <- "../output"
+  outstub <- "../output/"
+  
+  est <- fread(file.path(instub, "estimates_autocorrelation.csv"))
+  est_var <- est[var != "cumsum_from0"]
+  
+  
+  txt <- c("<tab:static>")
+  
+  txt <- c(txt, 
+           paste(est_var[model == "levels_model" & var == "mw_wkp_tot_17"]$b, 
+                 est_var[model == "baseline_model" & var == "mw_wkp_tot_17"]$b, sep = "\t"), 
+           paste(est_var[model == "levels_model" & var == "mw_wkp_tot_17"]$se,
+                 est_var[model == "baseline_model" & var == "mw_wkp_tot_17"]$se, sep = "\t"))
+  
+  txt <- c(txt, 
+           paste(est_var[model == "levels_model" & var == "mw_res"]$b, 
+                 est_var[model == "baseline_model" & var == "mw_res"]$b, sep = "\t"), 
+           paste(est_var[model == "levels_model" & var == "mw_res"]$se, 
+                 est_var[model == "baseline_model" & var == "mw_res"]$se, sep = "\t"))
+  
+  txt <- c(txt, paste(est_var[model == "baseline_model" & var == "mw_res"]$p_val))
+  
+  txt <- c(txt, paste(est_var[model == "levels_model" & var == "mw_res"]$N, 
+                      est_var[model == "baseline_model" & var == "mw_res"]$N, sep = "\t"))
+  
+  fileConn <- file(file.path(outstub, "autocorrelation.txt"))
+  writeLines(txt, fileConn)
+  close(fileConn)
+}
+
+main()
