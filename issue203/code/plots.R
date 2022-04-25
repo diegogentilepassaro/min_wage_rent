991d68bfc7b6194cc430169ac561948f74d88b61
santiagohermo <santiagohermo@users.noreply.github.com>
2022-04-24 18:15:31 -0700

    Changes to probability renting plot #203

--- issue203/code/plots.R
+++ issue203/code/plots.R
@@ -12,18 +12,19 @@ sh_rent <- data %>%
   filter((is_owner + is_tenant) == 1,
          !is.na(hh_income),
          house_apartment_unit == 1) %>%
-  mutate(hh_income_decile = ntile(resid(lm(hh_income ~ 1 + factor(smsa), data = .)), 10)) %>%
+  group_by(smsa) %>%
+  mutate(hh_income_decile = ntile(hh_income, 10)) %>%
+  ungroup %>%
   group_by(hh_income_decile) %>%
-  summarise(mean = mean(is_tenant)) %>%
-  ggplot(aes(x = factor(hh_income_decile), y = mean)) +
+  summarise(pr_tenant = mean(is_tenant)) %>%
+  ggplot(aes(x = factor(hh_income_decile), y = pr_tenant)) +
   geom_bar(stat = 'identity', fill = "#A64253") +
   theme_bw() +
   guides(fill = 'none') +
-  xlab('Household income decile') +
-  ylab('Share renter households') +
-  coord_cartesian(ylim = c(0, 1)) +
-  theme(panel.grid.major.x = element_blank()) +
-  scale_y_continuous(labels = scales::percent_format(accuracy = 1))
+  xlab('Household income decile (within SMSA)') +
+  ylab('Probability housing is rented') +
+  coord_cartesian(ylim = c(0, 0.8)) +
+  theme(panel.grid.major.x = element_blank())
 
 ggsave('../output/share_renters.png', sh_rent, width = 2221, height = 1615, dpi=300, 
        units = 'px')
