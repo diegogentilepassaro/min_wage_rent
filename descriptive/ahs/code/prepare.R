7492f1eaa8672e0c0214dfd54b9b7212bcdfe542
Martín Gallardo <69550249+martingallardo23@users.noreply.github.com>
2022-05-09 20:07:14 -0300

    Update code and plots #207

--- descriptive/ahs/code/prepare.R
+++ descriptive/ahs/code/prepare.R
@@ -11,19 +11,23 @@ main <- function() {
   
   hh_data <- hh_data %>% 
     filter(is_owner == 1 | is_tenant == 1, 
-           !is.na(hh_income),
+           hh_income > 0,
            house_apartment_unit == 1) 
   
   person_data <- read_csv(file.path(in_data, 'person_2011_2013.csv'))
   
   sh_renters <- hh_data %>%
-    mutate(hh_income_decile = ntile(resid(lm(hh_income ~ factor(smsa) , data = .)), 10)) %>%
+    mutate(hh_income_res      = resid(lm(hh_income ~ factor(smsa) , data = .)),
+           hh_income_decile   = ntile(hh_income_res, 10),
+           pr_tenant_res      = resid(lm(is_tenant ~ factor(smsa) , data = .)),
+           pr_tenant          = pr_tenant_res + mean(is_tenant)) %>%
     group_by(hh_income_decile) %>%
-    summarise(pr_tenant = mean(is_tenant))
+    summarise(pr_tenant = mean(pr_tenant))
   
   sh_unit_types <- hh_data %>%
     filter(is_tenant == 1) %>% 
-    mutate(hh_income_decile = ntile(resid(lm(hh_income ~ factor(smsa) , data = .)), 10)) %>%
+    mutate(hh_income_res      = resid(lm(hh_income ~ factor(smsa) , data = .)),
+           hh_income_decile   = ntile(hh_income_res, 10)) %>%
     group_by(hh_income_decile, n_units_cat) %>%
     summarise(count_unit_type = n()) %>%
     group_by(hh_income_decile) %>%
@@ -31,21 +35,28 @@ main <- function() {
     select(-count_unit_type)
   
   sh_condo <- hh_data %>%
-    filter(is_tenant == 1, 
-           hh_income >  0) %>% 
-    mutate(hh_income_decile = ntile(resid(lm(hh_income ~ factor(smsa) , data = .)), 10)) %>%
+    filter(is_tenant == 1) %>% 
+    mutate(hh_income_res      = resid(lm(hh_income ~ factor(smsa) , data = .)),
+           hh_income_decile   = ntile(hh_income_res, 10),
+           sh_condo_res       = resid(lm(is_condo_coop ~ factor(smsa) , data = .)),
+           sh_condo           = sh_condo_res + mean(is_condo_coop)) %>%
     group_by(hh_income_decile) %>%
-    summarise(sh_condo = mean(is_condo_coop))
+    summarise(sh_condo = mean(sh_condo))
   
   sh_hh_head <- person_data %>%
-    left_join(hh_data[, c('household_id', 'is_tenant', 'hh_income')], by = 'household_id') %>%
+    left_join(hh_data[, c('household_id', 'is_tenant', 'house_apartment_unit')], 
+              by = 'household_id') %>%
     filter(is_tenant == 1,
-      person_salary > 0) %>% # Está bien hacer esto?
+           house_apartment_unit == 1,
+      person_salary > 0) %>% 
     group_by(household_id) %>%
     mutate(max_income   =  max(person_salary),
            hh_head_max  =  1 * (person_salary == max_income)) %>%
-    group_by(smsa) %>%
-    mutate(person_salary_decile = ntile(person_salary, 10)) %>%
+    ungroup %>% 
+    mutate(person_salary_res      = resid(lm(person_salary ~ factor(smsa) , data = .)),
+           person_salary_decile   = ntile(person_salary_res, 10),
+           hh_head_max_res        = resid(lm(hh_head_max ~ factor(smsa) , data = .)),
+           hh_head_max            = hh_head_max_res + mean(hh_head_max)) %>%
     group_by(person_salary_decile) %>%
     summarise(sh_hh_head_max = mean(hh_head_max))
   
