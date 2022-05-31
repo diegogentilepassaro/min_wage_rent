b153ae27edd13e8a45f8217055b8d65b48972067
Martín Gallardo <69550249+martingallardo23@users.noreply.github.com>
2022-05-31 11:05:51 -0300

    Implement Santiago's comments #212

--- descriptive/ahs/code/prepare.R
+++ descriptive/ahs/code/prepare.R
@@ -11,6 +11,8 @@ main <- function() {
   
   hh_data_renters <- load_hh_data(in_data, TRUE)
   
+  hh_data_rent_sqft <- load_hh_data(in_data, TRUE, TRUE)
+  
   person_data <- load_person_data(in_data, hh_data)
   
   sh_renters <- hh_data %>%
@@ -35,21 +37,21 @@ main <- function() {
     summarise(sh_condo = mean(sh_condo),
               n = unique(n))
   
-  avg_sqft <- hh_data_renters %>%
+  avg_sqft <- hh_data_rent_sqft %>%
     mutate(unit_sqft_res      = resid(lm(unit_sqft ~ factor(smsa) , data = ., na.action=na.exclude)),
            unit_sqft          = unit_sqft_res + mean(unit_sqft, na.rm=T)) %>%
     group_by(hh_income_decile) %>%
     summarise(avg_sqft = mean(unit_sqft, na.rm=T),
               n = unique(n))
   
-  avg_rent <- hh_data_renters %>%
+  avg_rent <- hh_data_rent_sqft %>%
     mutate(monthly_hh_rent_res = resid(lm(monthly_hh_rent ~ factor(smsa) , data = ., na.action=na.exclude)),
            monthly_hh_rent     = monthly_hh_rent_res + mean(monthly_hh_rent, na.rm=T)) %>%
     group_by(hh_income_decile) %>%
     summarise(avg_rent = mean(monthly_hh_rent, na.rm=T),
               n = unique(n))
   
-  avg_rent_psqft <- hh_data_renters %>%
+  avg_rent_psqft <- hh_data_rent_sqft %>%
     mutate(rent_psqft     = monthly_hh_rent / unit_sqft, 
            rent_psqft_res = resid(lm(rent_psqft ~ factor(smsa) , data = ., na.action=na.exclude)),
            rent_psqft     = rent_psqft_res + mean(rent_psqft, na.rm=T)) %>%
@@ -78,26 +80,29 @@ main <- function() {
   
 }
 
-load_hh_data <- function(path, renters_only = FALSE) {
+load_hh_data <- function(path, renters_only = FALSE, rent_sqft = FALSE) {
   data <- read_csv(file.path(path, 'household_2011_2013.csv'))
   
   data <- data %>%
     filter(is_owner == 1 | is_tenant == 1,
            hh_income > 0,
-           house_apartment_unit == 1) %>%
+           house_apartment_unit == 1)
+  
+  if (renters_only) {
+    data <- data %>%
+      filter(is_tenant == 1)
+  }
+  
+  if (rent_sqft) {
+    data <- data %>%
+      filter(!is.na(unit_sqft),
+             !is.na(monthly_hh_rent))
+  }
+  
+  data <- data %>% 
     mutate(hh_income_res      = resid(lm(hh_income ~ factor(smsa) , data = .)),
            hh_income_decile   = ntile(hh_income_res, 10),
            n = n())
-  
-  if (renters_only == TRUE) {
-    data <- data %>%
-      filter(is_tenant == 1,
-             !is.na(unit_sqft),
-             !is.na(monthly_hh_rent)) %>%
-      mutate(hh_income_res      = resid(lm(hh_income ~ factor(smsa) , data = .)),
-             hh_income_decile   = ntile(hh_income_res, 10),
-             n = n())
-  }
   return(data)
 }
 
