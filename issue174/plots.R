cb5c4dbe40a99049b4c7232ae31ba938ae90ea16
santiagohermo <santiagohermo@users.noreply.github.com>
2022-06-20 07:34:41 -0700

    forgotten

--- issue174/plots.R
+++ issue174/plots.R
@@ -19,55 +19,64 @@ for (tipo in c("cbsa_month", "month")) {
   
   ## Workplace MW
   
-  dt[, d_mw_wkp_resid_mw_res_dec := resid(
-    feols(d_mw_wkp ~ -1 | mw_res_100groups, dt)
+  dt[, mw_wkp_resid_mw_res_dec := resid(
+    feols(mw_wkp ~ -1 | zipcode + mw_res_100groups, dt)
   )]
-  dt[, d_ln_rents_resid_mw_res_dec := resid(
-    feols(d_ln_rents ~ -1 | mw_res_100groups, dt)
+  dt[, ln_rents_resid_mw_res_dec := resid(
+    feols(ln_rents ~ -1 | zipcode + mw_res_100groups, dt)
   )]
   
   for (stub in c("", "_resid_mw_res_dec")) {
     
-    ggplot(dt[d_mw_wkp > 0], 
-           aes_string(x = paste0("d_mw_wkp", stub), 
-                      y = paste0("d_ln_rents", stub))) +
+    ggplot(dt, 
+           aes_string(x = paste0("mw_wkp", stub), 
+                      y = paste0("ln_rents", stub))) +
       geom_point(alpha = 0.15) +
       geom_smooth(formula = "y ~ x", method = "lm") + 
       stat_summary_bin(fun = "mean", 
                        breaks = quantile(dt[[paste0("d_mw_wkp", stub)]], 
                                          probs = 0:10/10), 
                        color = "red") +
-      coord_cartesian(ylim = c(-0.05, 0.05)) +
-      theme_bw()
+      theme_bw() -> plt
+    
+    if (grepl("resid", stub)) {
+      plt <- plt + coord_cartesian(ylim = c(-0.2,  0.2), 
+                                   xlim = c(-0.15, 0.15))
+    }
     
     ggsave(paste0("plots/", tipo, "_mw_wkp", stub, ".png"),
            height = 5, width = 7)
   }
   
-  ## Workplace MW
+  ## Residence MW
   
-  dt[, d_mw_res_resid_mw_wkp_dec := resid(
-    feols(d_mw_res ~ -1 | mw_wkp_100groups, dt)
+  dt[, mw_res_resid_mw_wkp_dec := resid(
+    feols(mw_res ~ -1 | zipcode + mw_wkp_100groups, dt)
   )]
-  dt[, d_ln_rents_resid_mw_wkp_dec := resid(
-    feols(d_ln_rents ~ -1 | mw_wkp_100groups, dt)
+  dt[, ln_rents_resid_mw_wkp_dec := resid(
+    feols(ln_rents ~ -1 | zipcode + mw_wkp_100groups, dt)
   )]
   
   for (stub in c("", "_resid_mw_wkp_dec")) {
     
     ggplot(dt, 
-           aes_string(x = paste0("d_mw_res", stub), 
-                      y = paste0("d_ln_rents", stub))) +
+           aes_string(x = paste0("mw_res", stub), 
+                      y = paste0("ln_rents", stub))) +
       geom_point(alpha = 0.15) +
       geom_smooth(formula = "y ~ x", method = "lm") + 
       stat_summary_bin(fun = "mean", 
                        breaks = quantile(dt[[paste0("d_mw_res", stub)]], 
                                          probs = 0:10/10), 
                        color = "red") +
-      coord_cartesian(ylim = c(-0.05, 0.05)) +
-      theme_bw()
+      theme_bw() -> plt
+    
+    if (grepl("resid", stub)) {
+      plt <- plt + coord_cartesian(ylim = c(-0.2,  0.2), 
+                                   xlim = c(-0.15, 0.15))
+    }
     
     ggsave(paste0("plots/", tipo, "_mw_res", stub, ".png"),
            height = 5, width = 7)
   }
 }
+
