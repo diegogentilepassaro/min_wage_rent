7e45e8b7bd21edd72e5bde42889d5d09cdd7e602
santiagohermo <santiagohermo@users.noreply.github.com>
2022-07-04 13:06:04 -0700

    Add loop in tables.R

--- analysis/counterfactuals/code/tables.R
+++ analysis/counterfactuals/code/tables.R
@@ -7,67 +7,39 @@ main <- function() {
   
   dt <- fread(file.path(instub, "data_counterfactuals.csv"))
   
-  dt_cf <- dt[counterfactual == "fed_9usd" 
-             & year == 2020 
-             & cbsa_low_inc_increase == 0]
+  for (cf in c("fed_9usd", "chi14")) {
 
-  txt <- c("<tab:counterfactuals_fed_9usd>")
+      dt_cf <- dt[counterfactual == cf
+                  & year == 2020 
+                  & cbsa_low_inc_increase == 0]
 
-  for (sample in c("fully_affected", "no_direct_treatment")) {
-    
-    dt_sample <- dt_cf[get(sample) == 1]
-    
-    txt <- c(txt, 
-            paste(dim(dt_sample)[1],
-                  median(dt_sample[["d_mw_res"]],         na.rm = T),
-                  median(dt_sample[["d_mw_wkp"]],         na.rm = T),
-                  median(dt_sample[["s_imputed"]],        na.rm = T),
-                  median(dt_sample[["rho_with_imputed"]], na.rm = T),
+      txt <- c(sprintf("<tab:counterfactuals_%s>", cf))
+
+      for (sample in c("fully_affected", "no_direct_treatment")) {
+      
+      dt_sample <- dt_cf[get(sample) == 1]
+      
+      txt <- c(txt, 
+                  paste(dim(dt_sample)[1],
+                        median(dt_sample[["d_mw_res"]],         na.rm = T),
+                        median(dt_sample[["d_mw_wkp"]],         na.rm = T),
+                        median(dt_sample[["s_imputed"]],        na.rm = T),
+                        median(dt_sample[["rho_with_imputed"]], na.rm = T),
+                        sep = "\t"))
+      }
+
+      dt_tot_incidence <- fread(file.path(instub, "tot_incidence.csv"))
+      dt_tot_incidence <- dt_tot_incidence[counterfactual == cf]
+      txt <- c(txt, 
+            paste(dt_tot_incidence$N,
+                  dt_tot_incidence$tot_incidence,
                   sep = "\t"))
+      
+      fileConn <- file(file.path(outstub, sprintf("counterfactuals_%s.txt", cf)))
+      writeLines(txt, fileConn)
+      close(fileConn)
   }
-
-  dt_tot_incidence <- fread(file.path(instub, "tot_incidence.csv"))
-  dt_tot_incidence <- dt_tot_incidence[counterfactual == "fed_9usd"]
-  txt <- c(txt, 
-        paste(dt_tot_incidence$N,
-              dt_tot_incidence$tot_incidence,
-              sep = "\t"))
-  
-  fileConn <- file(file.path(outstub, "counterfactuals_fed_9usd.txt"))
-  writeLines(txt, fileConn)
-  close(fileConn)
-
-  # Chicago increase to 14
-  dt_cf <- dt[counterfactual == "chi14" 
-              & year == 2020 
-              & cbsa_low_inc_increase == 0]
-  
-  txt <- c("<tab:counterfactuals_chi14>")
-  
-  for (sample in c("fully_affected", "no_direct_treatment")) {
-    
-    dt_sample <- dt_cf[get(sample) == 1]
-    
-    txt <- c(txt, 
-             paste(dim(dt_sample)[1],
-                   median(dt_sample[["d_mw_res"]],         na.rm = T),
-                   median(dt_sample[["d_mw_wkp"]],         na.rm = T),
-                   median(dt_sample[["s_imputed"]],        na.rm = T),
-                   median(dt_sample[["rho_with_imputed"]], na.rm = T),
-                   sep = "\t"))
-  }
-  
-  dt_tot_incidence <- fread(file.path(instub, "tot_incidence.csv"))
-  dt_tot_incidence <- dt_tot_incidence[counterfactual == "chi14"]
-  txt <- c(txt, 
-           paste(dt_tot_incidence$N,
-                 dt_tot_incidence$tot_incidence,
-                 sep = "\t"))
-  
-  fileConn <- file(file.path(outstub, "counterfactuals_chi14.txt"))
-  writeLines(txt, fileConn)
-  close(fileConn)
-  
+ 
   # Other Federal cfs
   
   txt <- c("<tab:counterfactuals_other>")
