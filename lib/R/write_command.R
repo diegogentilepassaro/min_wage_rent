07dc6c36061747702169eedb1a1e17b65f18e439
santiagohermo <santiagohermo@users.noreply.github.com>
2022-07-18 10:53:40 -0700

    Fix autofill of `fd_baseline` dropping 'textnormal' #237

--- lib/R/write_command.R
+++ lib/R/write_command.R
@@ -1,8 +1,19 @@
-write_command <- function(name, value, comment = NULL) {
-  if (is.null(comment)) {
-    return(paste0("\\newcommand{\\", name, "}{\\textnormal{", value, "}}\n"))
-  }
+write_command <- function(name, value, comment = NULL, textnormal = T) {
+
+  if (textnormal) {
+    if (is.null(comment)) {
+      return(paste0("\\newcommand{\\", name, "}{\\textnormal{", value, "}}\n"))
+    }
+    else {
+      return(paste0("\\newcommand{\\", name, "}{\\textnormal{", value, "}}\t%", comment,"\n"))
+    }
+  } 
   else {
-    return(paste0("\\newcommand{\\", name, "}{\\textnormal{", value, "}}\t%", comment,"\n"))
+    if (is.null(comment)) {
+      return(paste0("\\newcommand{\\", name, "}{", value, "}\n"))
+    }
+    else {
+      return(paste0("\\newcommand{\\", name, "}{", value, "}\t%", comment,"\n"))
+    }
   }
 }
